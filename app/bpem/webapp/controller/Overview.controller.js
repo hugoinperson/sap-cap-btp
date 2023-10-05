869fe30531db52c09fc48fdb8e82defe8e7d70be
Jerry Hsu <hsuyachieh@gmail.com>
2023-10-05 00:42:42 -0500

    add test api

--- app/bpem/webapp/controller/Overview.controller.js
+++ app/bpem/webapp/controller/Overview.controller.js
@@ -8,22 +8,22 @@ sap.ui.define([
   const _Controller = BaseController.extend(CLASS_NAME)
 
   _Controller.prototype.onInit = function () {
-  //   const model = this.getOwnerComponent().getModel('bpem')
-  //   const actionODataContextBinding = model.bindContext('/numberOfCases(...)')
-  //   actionODataContextBinding.setParameter('data', {
-  //     processor: 'jerry'
-  //   })
-  //   actionODataContextBinding.execute().then(
-  //     function() {
-  //       var actionContext = actionODataContextBinding.getBoundContext()
-  //       console.log(actionContext.getObject().value)
-  //     }.bind(this),
-  //     function (oError) {
-  //       MessageBox.alert(oError.message, {
-  //           icon : MessageBox.Icon.ERROR,
-  //           title : 'Error'})
-  //       }
-  //  )
+    const model = this.getOwnerComponent().getModel('bpem')
+    const actionODataContextBinding = model.bindContext('/numberOfCases(...)')
+    actionODataContextBinding.setParameter('data', {
+      processor: 'jerry'
+    })
+    actionODataContextBinding.execute().then(
+      function() {
+        var actionContext = actionODataContextBinding.getBoundContext()
+        console.log(actionContext.getObject().value)
+      }.bind(this),
+      function (oError) {
+        MessageBox.alert(oError.message, {
+            icon : MessageBox.Icon.ERROR,
+            title : 'Error'})
+        }
+   )
   }
 
   return _Controller
