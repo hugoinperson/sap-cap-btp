38698cd33c67d6dface9f34e51f9292a1e6fe028
Jerry Hsu <hsuyachieh@gmail.com>
2023-10-05 00:42:13 -0500

    add chat bot

--- app/bpem/webapp/Component.js
+++ app/bpem/webapp/Component.js
@@ -1,13 +1,13 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "sap/ui/Device"
+    'sap/ui/core/UIComponent',
+    'sap/ui/Device'
 ], function (UIComponent, Device) {
-    "use strict";
+    'use strict'
 
-    return UIComponent.extend("u4u.bpem.Component", {
+    return UIComponent.extend('u4u.bpem.Component', {
 
         metadata : {
-            manifest: "json"
+            manifest: 'json'
         },
 
         /**
@@ -18,13 +18,13 @@ sap.ui.define([
          */
         init : function () {
             // call the base component's init function
-            UIComponent.prototype.init.apply(this, arguments);
+            UIComponent.prototype.init.apply(this, arguments)
 
             // create the views based on the url/hash
-            this.getRouter().initialize();
+            this.getRouter().initialize()
             
             // if you are a manager
-            this.getRouter().navTo("overview");
+            this.getRouter().navTo('overview')
         },
 
         /**
@@ -35,7 +35,7 @@ sap.ui.define([
          */
         destroy : function () {
             // call the base component's destroy function
-            UIComponent.prototype.destroy.apply(this, arguments);
+            UIComponent.prototype.destroy.apply(this, arguments)
         },
 
         /**
@@ -47,19 +47,18 @@ sap.ui.define([
         getContentDensityClass : function() {
             if (this._sContentDensityClass === undefined) {
                 // check whether FLP has already set the content density class; do nothing in this case
-                // eslint-disable-next-line fiori-custom/sap-no-proprietary-browser-api, fiori-custom/sap-browser-api-warning
-                if (document.body.classList.contains("sapUiSizeCozy") || document.body.classList.contains("sapUiSizeCompact")) {
-                    this._sContentDensityClass = "";
+                if (document.body.classList.contains('sapUiSizeCozy') || document.body.classList.contains('sapUiSizeCompact')) {
+                    this._sContentDensityClass = ''
                 } else if (!Device.support.touch) { // apply "compact" mode if touch is not supported
-                    this._sContentDensityClass = "sapUiSizeCompact";
+                    this._sContentDensityClass = 'sapUiSizeCompact'
                 } else {
                     // "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
-                    this._sContentDensityClass = "sapUiSizeCozy";
+                    this._sContentDensityClass = 'sapUiSizeCozy'
                 }
             }
-            return this._sContentDensityClass;
+            return this._sContentDensityClass
         }
 
-    });
+    })
 
-});
+})
--- app/bpem/webapp/controller/App.controller.js
+++ app/bpem/webapp/controller/App.controller.js
@@ -1,13 +1,108 @@
 sap.ui.define([
-  './BaseController'
-], function (BaseController) {
+  './BaseController',
+  'sap/ui/core/Fragment',
+  'u4u/bpem/lib/WebChat'
+], function (BaseController, Fragment, WebChat) {
   'use strict'
 
-  return BaseController.extend('u4u.bpem.controller.App', {
+  const CLASS_NAME = 'u4u.bpem.controller.App'
 
-    onInit: function () {
-      // apply content density mode to root view
-      this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass())
+  const _Controller = BaseController.extend(CLASS_NAME)
+
+  _Controller.prototype.onInit = function () {
+    WebChat // to load the WebChat library
+    this._chatBotInitialized = false
+  }
+
+  _Controller.prototype.onPress = async function (event) {
+    const button = event.getSource()
+
+    if (!this._chatBotPopover) {
+      this._chatBotPopover = await this._getChatBotPopover()
     }
-  })
+    
+    this._chatBotPopover.openBy(button)
+
+    if (!this._chatBotInitialized) {
+      this._initChatBot()
+    }
+  }
+
+  _Controller.prototype._getChatBotPopover = function () { 
+    const view = this.getView()
+
+    return Fragment.load({
+      id: view.getId(),
+      name: 'u4u.bpem.fragment.ChatBot',
+      controller: this
+    }).then(function(oPopover) {
+      view.addDependent(oPopover)
+      return oPopover
+    })
+  }
+
+  _Controller.prototype._initChatBot = function () { 
+    const token = 'kxNiWcBc_2E.wm82ZFeYmRZ-sC-syginmWZ_rYldQJtMX76jn-efTtc'
+    const avatarOptions = {
+      botAvatarImage: 'https://docs.microsoft.com/en-us/azure/bot-service/v4sdk/media/logo_bot.svg?view=azure-bot-service-4.0',
+      botAvatarInitials: 'UT',
+      userAvatarImage: 'https://github.com/compulim.png?size=64',
+      userAvatarInitials: 'TN'
+    }
+    const store = window.WebChat.createStore({}, ({ dispatch }) => next => async action => {
+      if (action.type === 'DIRECT_LINE/CONNECT_FULFILLED') {
+        console.log('Chatbot event dispatched')
+        dispatch({
+          type: 'WEB_CHAT/SEND_EVENT',
+          payload: {
+            name: 'webchat/join',
+            value: {
+              language: window.navigator.language
+            }
+          }
+        })
+      } else if (action.type === 'DIRECT_LINE/INCOMING_ACTIVITY') {
+        if (action.payload.activity.from.role === 'bot') {
+          console.log('bot: ' + action.payload.activity.text)
+        }
+        if (action.payload.activity.from.role === 'user') {
+          console.log('user: ' + action.payload.activity.text)
+          let txt = action.payload.activity.text
+          if (txt === undefined) {
+            txt = ''
+          }
+        }
+      }
+
+      return next(action)
+    })
+
+    const styleSet = window.WebChat.createStyleSet({
+      // bubbleBackground: 'rgba(0, 0, 255, .1)',
+      // bubbleFromUserBackground: 'rgba(0, 255, 0, .1)',
+      rootHeight: '490px',
+      rootWidth: '100%',
+      backgroundColor: 'azure'
+    })
+
+    // After generated, you can modify the CSS rules.
+    // Change font family and weight.
+    styleSet.textContent = {
+      ...styleSet.textContent,
+      fontFamily: '\'Roboto\', \'Arial\', sans-serif',
+      fontWeight: 'regular'
+    }
+
+    window.WebChat.renderWebChat({
+      directLine: window.WebChat.createDirectLine({ token: token }),
+      store: store,
+      styleSet,
+      styleOptions: avatarOptions
+    }, document.getElementById('container-u4u.bpem---app--webchat'))
+
+    this._chatBotInitialized = true
+  }
+  
+
+  return _Controller
 })
